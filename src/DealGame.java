36083aaf29650112abc768c3ed19d3c32b35a5f4
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-16 22:33:40 +0100

    Program Deal no Deal

--- src/DealGame.java
+++ src/DealGame.java
@@ -7,18 +7,20 @@ public class DealGame {
     }
 
     /*
- * for each element of arrays output in [] either p, or index or nothing
- * for example, [p] [2] [] [4]
- */
-    public void display_suitecases(int [] cases, int own_case_index) {
+    * for each element of arrays output in [] either p, or index or nothing
+    * for example, [p] [2] [] [4]
+    */
+    public static void display_suitecases(int [] cases, int own_case_index) {
     }
 
-    // bank_offer
-    // arguments:  int [] cases
-    // return value: offer
-    // sum all elements and divide by number of non-zero elements + 1
-    public void bankOffer() {
-    
+    /*
+    * bank_offer
+    * arguments:  int [] cases
+    * return value: offer
+    * sum all elements and divide by number of non-zero elements + 1
+    */
+
+    public int bankOffer(int[] cases) {
     }
 
 
