6617d538fe6dc0a0d85a422e92c39ce11c47c9d8
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-16 22:24:09 +0100

    Initial commit

---
+++ src/DealGame.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Михаил on 16.01.2017.
+ */
+public class DealGame {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+ * for each element of arrays output in [] either p, or index or nothing
+ * for example, [p] [2] [] [4]
+ */
+    public void display_suitecases(int [] cases, int own_case_index) {
+    }
+
+    // bank_offer
+    // arguments:  int [] cases
+    // return value: offer
+    // sum all elements and divide by number of non-zero elements + 1
+    public void bankOffer() {
+    }
+
+
+
+}
