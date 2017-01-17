011ce4a8a74dab5e438c2764250cbd2fc851e42c
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-17 21:35:26 +0100

    Program Deal no Deal

--- src/DealGame.java
+++ src/DealGame.java
@@ -7,6 +7,8 @@ public class DealGame {
     public static void main(String[] args) {
 
         /* * 1) Distribute the suitcases, FillSuitcases method */
+        int[] arrayOfSuitcases = {6};
+        int[] arrayOfMoney = {6};
         fillSuitcases(arrayOfSuitcases, arrayOfMoney);
 
         /* 2) User selects a case; variable ownCaseIndex is set*/
@@ -25,7 +27,11 @@ public class DealGame {
         /*
          * 3) the game
          *  3.1 ask user to open a case he does not own, index of suitcase opened
-         *  3.2 open selected case: assigned 0 to element
+         */
+        for (int i = 0; i <= 4; i++) {
+        }
+        /*
+         *3.2 open selected case: assigned 0 to element
          *  3.3 get bank offer, bankOffer method
          *  3.4 temps user with the offer for his suitcase
          *  3.5 if user accepted the offer  - the game is over. if not continue the for loop.
