d95c2b98a6d8a7ee67e82e3f8b310f28649ad5bf
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-22 16:36:46 +0100

    Program Deal no Deal

---
+++ src/DealNoDeal.java
@@ -0,0 +1,53 @@
+/**
+ * Created by Михаил on 22.01.2017.
+ */
+public class DealNoDeal {
+
+    /**
+     * The game
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("");
+        /* 1) Create and fill suitcases */
+
+        /* 2) Ask user for his suitcaes, save in variable userCaseIndex */
+
+
+         /* 3) Ask user to open a suitcase, it must be a new suitcase. Repeat if input is wrong */
+
+
+         /* 4) Offer a user with BankOffer method
+    }
+
+
+    /**
+     * Fill arrayOfSuitcases with money randomly
+     * @param arrayOfSuitcaes
+     * @param arrayOfMoney
+     */
+    public void fillSuitcases(int[] arrayOfSuitcaes, int[] arrayOfMoney) {
+
+    }
+
+    /**
+     * Display an array of cases with [p] if case is owned by user, [] if case is eliminated from the game
+     * @param araySuitcases
+     * @param userCaseIndex
+     */
+    public void displaySuitcases(int[] araySuitcases, int userCaseIndex) {
+
+    }
+
+
+    /**
+     * Create an offer by dividing sum of all unopened suitcases on amount of unopened cases plus 1
+     * @param arrayOfSuitcases
+     */
+    public void bankOffer(int[] arrayOfSuitcases) {
+
+    }
+}
+
+
+
---
+++ src/HRMApplication.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Михаил on 20.01.2017.
+ */
+public class HRMApplication {
+
+    public HRMApplication(){
+
+    }
+    /*
+
+     */
+}
