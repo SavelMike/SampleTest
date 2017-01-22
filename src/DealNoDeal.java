38211e83a32a3fb5e12e790ce2dbb60481718b53
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-22 17:53:02 +0100

    Program Deal no Deal

--- src/DealNoDeal.java
+++ src/DealNoDeal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Михаил on 22.01.2017.
  */
@@ -11,15 +13,25 @@ public class DealNoDeal {
 
         /* 1) Create and fill suitcases */
         int[] arrayOfSuitcases = new int[6];
-        int[] attayOfMoney = {1, 20, 1000, 2000, 5000};
-        
-        /* 2) Ask user for his suitcases, save in variable userCaseIndex */
+        int[] arrayOfMoney = {1, 20, 1000, 2000, 5000};
+        fillSuitcases(arrayOfSuitcases, arrayOfMoney);
 
-         /* 3) Ask user to open a suitcase, it must be a new suitcase. Repeat if input is wrong */
+         /* 2) Ask user for his suitcases, save in a variable userCaseIndex */
+        int userCaseIndex = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please select the suitcase that you would like to own (1-6): ");
+        userCaseIndex = scanner.nextInt();
+        if ((userCaseIndex > 6) || (userCaseIndex < 0)) {
+            continue;
+        }
 
-         /* 4) Offer a user with BankOffer method, if Yes, game is over */
+         /* 3) Game loop contains 4 steps:
 
-         /* 5) If there is last element in array, Offer user to swap his case to lat case */
+         /*  3.1) Ask user to open a suitcase, it must be a new suitcase. Repeat if input is wrong */
+
+         /*  3.2) Offer a user with BankOffer method, if Yes, game is over */
+
+         /*  3.3) If there is last element in array, offer user to swap his case to last case */
     }
 
 
@@ -28,16 +40,16 @@ public class DealNoDeal {
      * @param arrayOfSuitcaes
      * @param arrayOfMoney
      */
-    public void fillSuitcases(int[] arrayOfSuitcaes, int[] arrayOfMoney) {
+    public static void fillSuitcases(int[] arrayOfSuitcaes, int[] arrayOfMoney) {
 
     }
 
     /**
-     * Display an array of cases with [p] if case is owned by user, [] if case is eliminated from the game
+     * Display an array of cases with [p] if case is owned by user, [] if case is eliminated from the game, and [index] for rest of elements
      * @param araySuitcases
      * @param userCaseIndex
      */
-    public void displaySuitcases(int[] araySuitcases, int userCaseIndex) {
+    public static void displaySuitcases(int[] araySuitcases, int userCaseIndex) {
 
     }
 
@@ -46,7 +58,7 @@ public class DealNoDeal {
      * Create an offer by dividing sum of all unopened suitcases on amount of unopened cases plus 1
      * @param arrayOfSuitcases
      */
-    public void bankOffer(int[] arrayOfSuitcases) {
+    public static void bankOffer(int[] arrayOfSuitcases) {
 
     }
 }
