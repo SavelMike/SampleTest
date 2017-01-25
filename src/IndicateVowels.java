4b0dea712842643c25d544a6391b3029562971a7
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-25 21:15:23 +0100

    IndicateVowels program

---
+++ src/IndicateVowels.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Михаил on 25.01.2017.
+ */
+public class IndicateVowels {
+
+    /**
+     * Take input from user by Scanner, call method isVowel, count the number of vowels
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Indicate vowels from user's input, return true if there are vowels
+     * @param userInput
+     * @return true if vowel
+     */
+    public static boolean isVowel(char userInput) {
+
+    }
+}
