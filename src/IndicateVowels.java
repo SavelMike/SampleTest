43acdf96a6ce886917279f3089989c1555ab90b1
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-25 21:42:16 +0100

    IndicateVowels program

--- src/IndicateVowels.java
+++ src/IndicateVowels.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+
 /**
  * Created by Михаил on 25.01.2017.
  */
 public class IndicateVowels {
 
     /**
-     * Take input from user by Scanner, call method isVowel, count the number of vowels
+     * Take input from user by Scanner save in nextLine, call method isVowel, count the number of vowels
      * @param args
      */
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input your sentence: ");
+        String str = sc.nextLine();
+        int cnt = 0;
+        for (int i = 0; i < str.length(); i ++) {
+            if (isVowel(str.charAt(i))) {
+                cnt++;
+            }
+        }
+        System.out.println("This sentence contains " + cnt + " vowels.");
     }
 
     /**
@@ -17,6 +28,11 @@ public class IndicateVowels {
      * @return true if vowel
      */
     public static boolean isVowel(char userInput) {
-
+        if (('a' == userInput) || ('e' == userInput) || ('i' == userInput) || ('o' == userInput) || ('u' == userInput) || ('y' == userInput)
+                || ('A' == userInput) || ('E' == userInput) || ('I' == userInput) || ('O' == userInput) || ('U' == userInput) || ('Y' == userInput)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
