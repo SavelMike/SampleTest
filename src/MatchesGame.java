07f78d776875869d58c98192c67f6b65ad860e95
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-25 18:37:59 +0100

    Program matches flow

--- src/MatchesGame.java
+++ src/MatchesGame.java
@@ -10,12 +10,11 @@ public class MatchesGame {
         int numOfMatches = 21;
         while (true) {
 
-            /* check whether there only 1 match left, if it is - user 1 lost */
+            /* check whether there only 1 match left, if it is - player 1 lost */
             if (numOfMatches == 1) {
                 System.out.println("Player 1 lost");
                 break;
             }
-
             int playerChoice;
             while (true) {
                 /* get player 1 input */
@@ -35,18 +34,20 @@ public class MatchesGame {
                     continue;
                 }
             }
+
             numOfMatches = numOfMatches - playerChoice;
             displayFlow(numOfMatches);
 
+            /* check whether there only 1 match left, if it is - player 2 lost */
+            if (numOfMatches == 1) {
+                System.out.println("Player 2 lost");
+                break;
+            }
 
             /* get player 2 input */
             while (true) {
                 System.out.print("Player 2, how many matches so you take (1, 2 or 3)? ");
                 playerChoice = scanner.nextInt();
-                if (numOfMatches == 1) {
-                    System.out.println("Player 2 lost");
-                    break;
-                }
 
                 if (numOfMatches == 3) {
                     if (playerChoice == 3) {
@@ -58,26 +59,20 @@ public class MatchesGame {
                         continue;
                     }
                 }
-
-                numOfMatches = numOfMatches - playerChoice;
-                displayFlow(numOfMatches);
+                if (playerChoice > 3) {
+                    continue;
+                }
             }
+            numOfMatches = numOfMatches - playerChoice;
+            displayFlow(numOfMatches);
         }
     }
-
 
     public static void displayFlow(int numOfMatches) {
         for (int i = 0; i < numOfMatches; i++) {
             System.out.print(" |");
         }
         System.out.println(" (" + numOfMatches + ")");
-    }
-
-    public static boolean isGameOver(int numOfMatches) {
-        if (numOfMatches == 1) {
-            return true;
-        }
-        return false;
     }
 }
 
