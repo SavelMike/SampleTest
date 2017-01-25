bbe00709c7770875846b4c449b7dd1c04f9b5c1a
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-25 17:50:53 +0100

    Program matches flow

--- src/MatchesGame.java
+++ src/MatchesGame.java
@@ -12,29 +12,33 @@ public class MatchesGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int numOfMatches = 21;
-        while (!isGameOver(numOfMatches)) {
-            System.out.print("Player 1, how many matches do you take (1, 2 or 3)? ");
+        while (true) {
 
-            /* get user1 input */
             /* check whether there only 1 match left, if it is - user 1 lost */
             if (numOfMatches == 1) {
                 System.out.println("Player 1 lost");
-                System.exit(1);
+                break;
             }
-            int player1Choice = scanner.nextInt();
-            numOfMatches = numOfMatches - player1Choice;
-            displayFlow(numOfMatches);
 
-            System.out.print("Player 2, how many matches so you take (1, 2 or 3)? ");
+            /* get user1 input */
+            System.out.print("Player 1, how many matches do you take (1, 2 or 3)? ");
+            int playerChoice = scanner.nextInt();
+            if (playerChoice > 3) {
+                continue;
+            }
+            numOfMatches = numOfMatches - playerChoice;
+            displayFlow(numOfMatches);
 
             /* get user2 input */
             /* check whether there only 1 match left, if it is - user 2 lost */
             if (numOfMatches == 1) {
                 System.out.println("Player 2 lost");
-                System.exit(0);
+                break;
             }
-            int player2Choice = scanner.nextInt();
-            numOfMatches = numOfMatches - player2Choice;
+
+            System.out.print("Player 2, how many matches so you take (1, 2 or 3)? ");
+            playerChoice = scanner.nextInt();
+            numOfMatches = numOfMatches - playerChoice;
             displayFlow(numOfMatches);
         }
     }
