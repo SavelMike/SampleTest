e4a9b02935a449724f6bf5094aebb75b0e125bc8
Saveliev Mikhail <Savelievmv@gmail.com>
2017-01-26 20:37:22 +0100

    Hangman program

--- src/Hangman.java
+++ src/Hangman.java
@@ -7,25 +7,32 @@ public class Hangman {
 
     public static void main(String[] args) {
 
-        /*  define word for be figured out as a constant  */
-        String word = "simple";
+        /*  define word for be figured out as a constant, create an array of 0, initialize variable attempts  */
+        String word = "simplification";
+        int[] opened = new int[14];
+        int attempts = 6;
+        for (int i = 0; i < opened.length; i++) {
+            opened[i] = 0;
+        }
 
-        while (gameIsOver() == false) {
+        while (!gameIsOver(opened)) {
             /* get char from user input */
             Scanner sc = new Scanner(System.in);
             System.out.println("Input your letter, please: ");
             String l = sc.next();
-            if (markOpenedChars()) {
-                    /* good attempt */
+            if (markOpenedChars(opened, word, l)) {
+                /* good attempt */
                 continue;
             } else {
-                /* If attempts equal 0, player lost. if not attempts minus 1; */
+                /* If attempts equal 0, player lost. if not attempts minus 1 */
                 if (attempts == 0) {
-                    player lost;
+                    System.out.println("No more attempts, you lost.");
+                    System.exit(0);
                 }
                 attempts--;
             }
         }
+        System.out.println("Congratulations, You won!");
     }
 
     /**
@@ -40,14 +47,18 @@ public class Hangman {
     }
 
     /**
-     * if word consist I, put 1 in all corespondent elements in opened[]
+     * if word consist l, put 1 in all corespondent elements in opened[]
      * @param opened
      * @param word
      * @param l
-     * return true if char I presents in word
+     * return true if ch l presents in word
      */
-    public static boolean markOpenedChars(int[] opened, String word, char l) {
-
+    public static boolean markOpenedChars(int[] opened, String word, String l) {
+        for (int i = 0; i < opened.length; i++) {
+            if (word.charAt(i) == l.charAt(i)) {
+                opened[i] = 1;
+            }
+        }
     }
 
     /**
